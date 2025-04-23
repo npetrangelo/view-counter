e282b9bb0a2edfc69742b71a5a29930b9ce20a0a
Nathan Petrangelo <nathan@petrangelo.org>
2025-04-23 16:52:54 -0700

    POST method adds entries to DB, GET method reads as well

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 This is a toy project to explore writing a stateless microservice with the Redis cache and a Database.
 
 The code here should be easily transferrable to a serverless function, albeit with some configuration changes to connect to the right cache and DB.
+
+
+To run this code,
+1. [Install Node](https://nodejs.org/en/download)
+2. [Install (and run) Postgres](https://postgresapp.com/downloads.html)
+3. [Install Docker](https://docs.docker.com/engine/install/)
+4. [Run Redis with Docker](https://redis.io/docs/latest/operate/oss_and_stack/install/install-stack/docker/)
+5. Clone this repository
+6. Run `node index.js` in your command line
--- index.js
+++ index.js
@@ -2,30 +2,58 @@ import express from 'express';
 import redis from 'redis';
 import pg from 'pg'
 
+const refreshRate = 10;
+
 const app = express();
 const port = 3000;
 
-const { Client } = pg
-const db = new Client()
-await db.connect()
+const { Client } = pg;
+const db = new Client();
+await db.connect();
+
+await db.query(`DROP TABLE IF EXISTS views`)
+
+await db.query(`CREATE TABLE views (
+  id VARCHAR(255),
+  count INTEGER
+);`);
+
+const createView = (id) => db.query(`INSERT INTO views (id, count) VALUES ('${id}', 1)`);
+const updateView = (id, count) => db.query(`UPDATE views SET count = ${count} WHERE id = ${id}`);
+const selectView = (id) => db.query(`SELECT count FROM views WHERE id = ${id}`);
 
 const cache = await redis.createClient()
   .on('error', err => console.log('Redis Client Error', err))
   .connect();
 
 app.get('/', async (req, res) => {
-    const value = await cache.get('key');
-    res.send('Hello World! ' + value);
+    res.send('Use /views/:id to access the views API!');
 });
 
-app.post('/:value', async (req, res) => {
-    await cache.set('key', req.params['value']);
-    res.send(req.params);
+app.get('/views/:id', async (req, res) => {
+    const { id } = req.params;
+    const views = selectView(id);
+    res.send(views);
 });
 
 app.post('/views/:id', async (req, res) => {
-    await cache.set('id', req.params['id']);
-    res.send('Hello World! ' + value);
+    const { id } = req.params;
+    const views = await createView(id);
+    res.send(views);
+});
+
+app.put('/views/:id', async (req, res) => {
+    const { id } = req.params;
+    const refresh = await cache.get(`refresh-${id}`);
+    if (refresh <= 0) {
+        const views = await cache.get(id);
+        await cache.set('refresh', refreshRate);
+        await updateView(id, views);
+    } else {
+        await cache.decr('refresh');
+        await cache.incr(id);
+    }
+    console.log('Incremented view');
 });
 
 app.listen(port, () => {
